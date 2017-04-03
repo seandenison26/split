114f018a21ef7029e14bd0db9f04f9e5d9e42679
Ziggy <sean.denison26@gmail.com>
2017-04-02 22:33:15 -0500

    Edited split variables to match DB model

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,4 @@
-/**********************************************\
+/**********************************************\1
 Creates router and contains CRUD routes for API
 \**********************************************/
 
@@ -16,6 +16,8 @@ var rootDir = __dirname + '/../'
 
 module.exports = router;
 
+ 
+
 // routes index.html when get request is made to main host
 router.get('/', function(req, res) {
 	res.sendFile(path.join(rootDir, 'index.html'), function(err) {
@@ -27,9 +29,64 @@ router.get('/', function(req, res) {
 	});
 });
 
-//posts a new account to the database
-router.post('/createAccount', function(req, res) {
+//posts a login request and returns a user data file if username and password match
+router.post('/accountLogin', function(req, res) {
+	var login = req;
+	console.log(req);
+	db.query("SELECT password from Account where ? = username",login, function(err,rows) {
+		if (err) {
+		console.log(err);
+		return;
+		}
+		else {
+		console.log(rows);
+		}
+	});
+};
 
+
+//logs a json list of all Account usernames 
+router.get('/accountList', function(req, res) {
+	db.query("SELECT usernames from Account", function(err,rows) {
+		if (err) {
+		console.log(err);
+		return
+		}
+		else {
+		console.log(rows);
+		}
+	})
+});
+
+
+
+//posts a new Account to the database
+router.post('/createAccount', function(req, res) {
+	// parse account values for db
+	console.log(req);
+	req.body();
+	
+	//create newAccount variable
+	var newAccount = {
+		username: "",
+		password: "",
+		email: ""
+	};
+
+	//insert accountValue into db
+	db.query("INSERT into Accounts set ?", newAccount, function(err,result) {
+	if (err) {
+		console.log(err); 
+	}	
+	else {
+		console.log(result.username + "was successfully inserted!");
+		newAccount = result;
+		return;
+	}
+	});
+	
+	//will EVENTUALLY send a brand new accountData file
+	res.send(newAccount);
 });
 
 
@@ -41,6 +98,11 @@ router.post('/createSplit', function(req, res) {
 
 //posts a new Purchase to the database
 router.get('/createPurchase', function(req, res) {
+
+});
+
+//updates a Split
+router.put('/updateSplit', function(req,res) {
 
 });
 
